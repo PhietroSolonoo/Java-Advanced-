b21c386a71b6d8cd17de538c7cc582191f47001b
PhietroSolonoo <negociosphietro@gmail.com>
2026-03-11 23:13:11 -0300

    Aula-05 Inclusão do pacote mapper, exception e suas respectivas classes. Inclusão do ProdutoResponse

--- src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
@@ -1,15 +1,14 @@
 package br.com.fiap.api_rest.controller;
 
 import br.com.fiap.api_rest.dto.ProdutoRequest;
+import br.com.fiap.api_rest.dto.ProdutoResponse;
 import br.com.fiap.api_rest.model.Produto;
 import br.com.fiap.api_rest.service.ProdutoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,62 +16,49 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
-    @Autowired
-    private  ProdutoService produtoService;
+    private final ProdutoService produtoService;
 
-    //CRUD - Create, Read, Update, Delete
-    // POST, GET, PUT,  DELETE
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
 
     @PostMapping
-    public ResponseEntity<Produto> createProduto(@Valid @RequestBody ProdutoRequest produto){
+    public ResponseEntity<Produto> createProduto(@Valid @RequestBody ProdutoRequest produto) {
         Produto produtoSalvo = produtoService.create(produto);
         return new ResponseEntity<>(produtoSalvo, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Produto> readProduto(@PathVariable UUID id){
-        Produto produto = produtoService.read(id);
-        if (produto == null){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<ProdutoResponse> readProduto(@PathVariable UUID id) {
+        ProdutoResponse produto = produtoService.read(id);
+        if (produto == null) {
+            return new ResponseEntity<>(produto, HttpStatus.NO_CONTENT);
         }
-            return new ResponseEntity<>(produto, HttpStatus.OK);
-
+        return new ResponseEntity<>(produto, HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity<List<Produto>> readProduto() {
-        List<Produto> produtos = produtoService.read();
+    public ResponseEntity<List<ProdutoResponse>> readProduto() {
+        List<ProdutoResponse> produtos = produtoService.read();
         if (produtos.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(produtos, HttpStatus.OK);
+        return new ResponseEntity<>(produtoService.read(), HttpStatus.OK);
     }
 
     @PutMapping
-    public ResponseEntity<Produto> updateProduto(@RequestBody Produto produto){
-        Produto produtoExistente = produtoService.read(produto.getId());
+    public ResponseEntity<Produto> updateProduto(@RequestBody Produto produto) {
+        ProdutoResponse produtoExistente = produtoService.read(produto.getId());
         if (produtoExistente == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        Produto produtoAtualizado = produtoService.uptade(produto);
+        Produto produtoAtualizado = produtoService.update(produto);
         return new ResponseEntity<>(produtoAtualizado, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduto(@PathVariable UUID id){
+    public ResponseEntity<Void> deleteProduto(@PathVariable UUID id) {
         produtoService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
-
-
-
     }
-
-
-
-    }
-
-
-
-
-
-
+}
--- src/main/java/br/com/fiap/api_rest/service/ProdutoService.java
+++ src/main/java/br/com/fiap/api_rest/service/ProdutoService.java
@@ -1,6 +1,8 @@
 package br.com.fiap.api_rest.service;
 
 import br.com.fiap.api_rest.dto.ProdutoRequest;
+import br.com.fiap.api_rest.dto.ProdutoResponse;
+import br.com.fiap.api_rest.mapper.ProdutoMapper;
 import br.com.fiap.api_rest.model.Produto;
 import br.com.fiap.api_rest.repository.ProdutoRepository;
 import org.springframework.beans.BeanUtils;
@@ -10,32 +12,43 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
-public class ProdutoService{
-    @Autowired
+public class ProdutoService {
     private ProdutoRepository produtoRepository;
+    private final ProdutoMapper produtoMapper;
 
-    // CRUD
-    public Produto create(ProdutoRequest produtoRequest){
+    @Autowired
+    public ProdutoService(ProdutoRepository produtoRepository, ProdutoMapper produtoMapper) {
+        this.produtoRepository = produtoRepository;
+        this.produtoMapper = produtoMapper;
+    }
+
+    public Produto create(ProdutoRequest produtoRequest) {
         Produto produto = new Produto();
         BeanUtils.copyProperties(produtoRequest, produto);
         return produtoRepository.save(produto);
-
     }
 
-    public Produto read(UUID id){
+    public ProdutoResponse read(UUID id) {
         Optional<Produto> produto = produtoRepository.findById(id);
-        return produto.orElse(null);
+        if (produto.isEmpty()) {
+            return null;
+        }
+        return produtoMapper.produtoToResponse(produto.get());
     }
 
-    public List<Produto> read(){
-        return produtoRepository.findAll();
+    public List<ProdutoResponse> read(){
+        List<Produto> produtos = produtoRepository.findAll();
+        return produtos
+                .stream()
+                .map(produtoMapper::produtoToResponse)
+                .collect(Collectors.toList());
     }
 
-    public Produto uptade(Produto produto) {
-        return  produtoRepository.save(produto);
-
+    public Produto update(Produto produto) {
+        return produtoRepository.save(produto);
     }
 
     public void delete(UUID id) {
