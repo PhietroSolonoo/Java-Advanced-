dcefe5abbbc34b26b57e5f94ca00a5c2e7473b00
PhietroSolonoo <negociosphietro@gmail.com>
2026-05-08 22:46:02 -0300

    Criando camada de segurança

--- src/main/java/br/com/fiap/api_rest/repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/api_rest/repository/UsuarioRepository.java
@@ -2,6 +2,7 @@ package br.com.fiap.api_rest.repository;
 
 import br.com.fiap.api_rest.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -9,6 +10,6 @@ import java.util.UUID;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
 
-    boolean findByLogin(String login);
+    UserDetails findByLogin(String login);
 
 }
--- src/main/java/br/com/fiap/api_rest/security/SecurityConfigurations.java
+++ src/main/java/br/com/fiap/api_rest/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.fiap.api_rest.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public DefaultSecurityFilterChain securityConfigurations(HttpSecurity httpSecurity) {
@@ -24,11 +28,13 @@ public class SecurityConfigurations {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth
+                                .requestMatchers(HttpMethod.GET, "")
                                 .requestMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                                 .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
     @Bean
